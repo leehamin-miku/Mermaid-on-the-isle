0bebcc4eb2fbfa8a4f86c1d8df38df2b2cc714ca
miku3984 <152942475+leehamin-miku@users.noreply.github.com>
2024-07-06 14:21:03 +0900

    실험중

--- MermaidProject/Assets/Scripts/Game/GameManager.cs
+++ MermaidProject/Assets/Scripts/Game/GameManager.cs
@@ -8,6 +8,6 @@ public class GameManager : MonoBehaviour
     // Start is called before the first frame update
     void Awake()
     {
-        PhotonNetwork.Instantiate("Prefab/Game/Player", new Vector3(0, 0, 0), Quaternion.identity, 0).GetComponent<Rigidbody2D>().AddForce(Vector2.up,ForceMode2D.Impulse);
+        PhotonNetwork.Instantiate("Prefab/Game/Player", new Vector3(0, 0, 0), Quaternion.identity, 0).GetComponent<Rigidbody2D>().AddForce(Random.insideUnitCircle*30,ForceMode2D.Impulse);
     }
 }
